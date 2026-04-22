716832c70d61279f64b917c61806a0b20e0e74e4
LeoDefossez <captainwater1@gmail.com>
2026-04-22 14:15:44 +0200

    Renaming tests resources, adapting configs, and adding a test for StringValues, currently ignored

--- src/test/java/dummies/ObjectArgsTypes.java
+++ src/test/java/dummies/ObjectArgsTypes.java
@@ -7,7 +7,7 @@ import java.util.List;
  * Create simple call stack for the JDIAttach when putting a breakpoint on the method dummies.ObjectArgsSimulation.endpoint(java.lang.String,int). 
  * This simulation demonstrate that not every arguments given to a method are references, in this case, the second parameter is an int
  */
-public class ObjectArgsSimulation {
+public class ObjectArgsTypes {
 
 	public static void main(String[] args) {
 		List<String> l = new ArrayList<>();
